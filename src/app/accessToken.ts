5f96aa8b3d7d761f7fe196f4332b037fd7c028be
AndreCalder <andre.calderonbarbeito@gmail.com>
2025-07-12 14:49:26 -0700

    Modified truthy access_token

--- src/app/accessToken.ts
+++ src/app/accessToken.ts
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-export const saveAccessToken = (token: string) => localStorage.setItem('access_token', token);
+export const saveAccessToken = (token: string) =>
+    localStorage.setItem("access_token", token);
 
-export const getAccessToken = () => localStorage.getItem('access_token' || "");
-
+export const getAccessToken = () => localStorage.getItem("access_token");
